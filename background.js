a4b42ba32c07ecbd309b3f366ccd76c840aa89a7
ShraddhaNandPandey <shraddhanandpandey1729@gmail.com>
2024-03-11 22:47:48 +0530

    added modal

--- background.js
+++ background.js
@@ -1,71 +1,82 @@
+// Access DOM elements
 const toggleButton = document.getElementById('mode-toggle');
 const body = document.body;
-let isDarkMode = false;
+const addNewButton = document.getElementById('addNew');
+const formSection = document.querySelector('.formSection');
+const closeFormButton = document.getElementById('closeFormButton');
+const applicationsTable = document.getElementById('applicationsTable');
 
-toggleButton.addEventListener('click', () => {
+// State variables
+let isDarkMode = false;
+let isEditing = false;
+
+// Event listeners
+toggleButton.addEventListener('click', toggleDarkMode);
+addNewButton.addEventListener('click', showForm);
+closeFormButton.addEventListener('click', hideForm);
+document.getElementById('save').addEventListener('click', saveApplication);
+document.querySelector('.search-bar input').addEventListener('input', filterApplications);
+window.onload = loadDataFromLocalStorage;
+
+// Functions
+function toggleDarkMode() {
     isDarkMode = !isDarkMode;
     body.classList.toggle('dark-mode');
     const imageSrc = isDarkMode ? 'images/moon.png': 'images/sun.png';
     toggleButton.setAttribute('src', imageSrc);
-});
+}
 
-const addNewButton = document.getElementById('addNew');
-const formSection = document.querySelector('.formSection');
-const closeFormButton = document.getElementById('closeFormButton');
-
-addNewButton.addEventListener('click', () => {
+function showForm() {
     formSection.style.display = 'block';
     closeFormButton.style.display = 'block';
-});
+}
 
-closeFormButton.addEventListener('click', () => {
+function hideForm() {
     formSection.style.display = 'none';
     closeFormButton.style.display = 'none';
-});
+    isEditing = false;
+}
 
-function addApplication() {
-    var companyName = document.getElementById('companyName').value;
-    var position = document.getElementById('position').value;
-    var applyDate = document.getElementById('applyDate').value;
-    var status = document.getElementById('status').value;
-    var link = document.getElementById('link').value;
-    var notes = document.getElementById('notes').value;
+function saveApplication() {
+    const companyName = document.getElementById('companyName').value;
+    const position = document.getElementById('position').value;
+    const applyDate = document.getElementById('applyDate').value;
+    const status = document.getElementById('status').value;
+    const link = document.getElementById('link').value;
+    const notes = document.getElementById('notes').value;
 
     if (!companyName || !position || !applyDate || !status || !link || !notes) {
         alert('Please fill in all fields');
         return;
     }
 
-    var table = document.getElementById('applicationsTable');
-    var newRow = table.insertRow(-1);
-
-    var cells = [];
-    for (var i = 0; i < 8; i++) {
+    const newRow = applicationsTable.insertRow(-1);
+    const cells = [];
+    for (let i = 0; i < 8; i++) {
         cells.push(newRow.insertCell(i));
     }
 
-    cells[0].innerText = table.rows.length - 1;
+    cells[0].innerText = applicationsTable.rows.length - 1;
     cells[1].innerText = companyName;
     cells[2].innerText = position;
     cells[3].innerText = applyDate;
     cells[4].innerText = status;
-    var linkElement = document.createElement('a');
+
+    const linkElement = document.createElement('a');
     linkElement.href = link;
     linkElement.target = '_blank';
-    var imageElement = document.createElement('img');
+    const imageElement = document.createElement('img');
     imageElement.src = 'images/link.png';
     linkElement.appendChild(imageElement);
     cells[5].appendChild(linkElement);
+
     cells[6].innerText = notes;
     cells[7].innerHTML = `<span class="material-symbols-outlined" onclick="editApplication(this)">edit</span> <span class="material-symbols-outlined" onclick="deleteApplication(this)">remove</span>`;
 
-    // Save data to local storage
     saveDataToLocalStorage();
-
     document.getElementById('applicationForm').reset();
+    hideForm();
 }
-
-var isEditing = false;
 
 function editApplication(button) {
     if (isEditing) {
@@ -73,8 +84,8 @@ function editApplication(button) {
         return;
     }
 
-    var row = button.parentNode.parentNode;
-    var cells = row.cells;
+    const row = button.parentNode.parentNode;
+    const cells = row.cells;
 
     document.getElementById('companyName').value = cells[1].innerText;
     document.getElementById('position').value = cells[2].innerText;
@@ -84,81 +95,95 @@ function editApplication(button) {
     document.getElementById('notes').value = cells[6].innerText;
 
     isEditing = true;
-    document.querySelector('.formSection').style.display = 'block';
+    formSection.style.display = 'block';
     row.parentNode.removeChild(row);
 
-    var tableRows = document.querySelectorAll("#applicationsTable tr:not(:first-child)");
-    for (var i = 0; i < tableRows.length; i++) {
-        tableRows[i].cells[0].innerText = i + 1;
-    }
+    const tableRows = document.querySelectorAll("#applicationsTable tr:not(:first-child)");
+    tableRows.forEach((row, index) => {
+        row.cells[0].innerText = index + 1;
+    });
 }
 
-document.getElementById('save').addEventListener('click', function() {
-    isEditing = false;
-    document.querySelector('.formSection').style.display = 'none';
-});
-
 function deleteApplication(button) {
-    var row = button.parentNode.parentNode;
+    const row = button.parentNode.parentNode;
     row.parentNode.removeChild(row);
-
-    // Save data to local storage after deletion
     saveDataToLocalStorage();
 }
 
-function changeText() {
-    var newName = prompt("Enter your name");
-    var headline = document.getElementById("headline");
-    if (newName) {
-        var imgTag = headline.querySelector('img');
-        localStorage.setItem('userName', newName);
-        headline.innerText = newName;
-    }
+function filterApplications() {
+    const searchText = this.value.toLowerCase();
+    const tableRows = document.querySelectorAll("#applicationsTable tr:not(:first-child)");
+
+    tableRows.forEach(row => {
+        const [companyName, position, applyDate, status, link, notes] = [...row.cells].map(cell => cell.innerText.toLowerCase());
+        const isVisible = [companyName, position, applyDate, status, link].some(cellContent => cellContent.includes(searchText));
+        row.style.display = isVisible ? '' : 'none';
+    });
 }
 
-// Function to save data to local storage
 function saveDataToLocalStorage() {
-    var tableRows = document.querySelectorAll("#applicationsTable tr:not(:first-child)");
-    var data = [];
-    for (var i = 0; i < tableRows.length; i++) {
-        var cells = tableRows[i].cells;
-        data.push({
-            companyName: cells[1].innerText,
-            position: cells[2].innerText,
-            applyDate: cells[3].innerText,
-            status: cells[4].innerText,
-            link: cells[5].querySelector('a').href,
-            notes: cells[6].innerText
-        });
-    }
+    const tableRows = document.querySelectorAll("#applicationsTable tr:not(:first-child)");
+    const data = Array.from(tableRows).map(row => ({
+        companyName: row.cells[1].innerText,
+        position: row.cells[2].innerText,
+        applyDate: row.cells[3].innerText,
+        status: row.cells[4].innerText,
+        link: row.cells[5].querySelector('a').href,
+        notes: row.cells[6].innerText
+    }));
     localStorage.setItem('tableData', JSON.stringify(data));
 }
 
-// Load data from local storage when the page loads
-window.onload = function () {
-    var storedName = localStorage.getItem('userName');
+function loadDataFromLocalStorage() {
+    const storedName = localStorage.getItem('userName');
     if (storedName) {
-        var headline = document.getElementById("headline");
-        headline.innerText = storedName;
+        document.getElementById("headline").innerText = storedName;
     }
 
-    var tableData = localStorage.getItem('tableData');
+    const tableData = localStorage.getItem('tableData');
     if (tableData) {
-        var data = JSON.parse(tableData);
-        for (var i = 0; i < data.length; i++) {
-            var newRow = document.getElementById('applicationsTable').insertRow(-1);
+        const data = JSON.parse(tableData);
+        data.forEach((item, index) => {
+            const newRow = applicationsTable.insertRow(-1);
             newRow.innerHTML = `
-                <td>${i + 1}</td>
-                <td>${data[i].companyName}</td>
-                <td>${data[i].position}</td>
-                <td>${data[i].applyDate}</td>
-                <td>${data[i].status}</td>
-                <td><a href="${data[i].link}" target="_blank"><img src="images/link.png"></a></td>
-                <td>${data[i].notes}</td>
+                <td>${index + 1}</td>
+                <td>${item.companyName}</td>
+                <td>${item.position}</td>
+                <td>${item.applyDate}</td>
+                <td>${item.status}</td>
+                <td><a href="${item.link}" target="_blank"><img src="images/link.png"></a></td>
+                <td>${item.notes}</td>
                 <td><span class="material-symbols-outlined" onclick="editApplication(this)">edit</span> <span class="material-symbols-outlined" onclick="deleteApplication(this)">remove</span></td>
             `;
-        }
+        });
     }
 }
-// add searching event listener
 
+// Modal code
+function openModal() {
+    const modal = document.getElementById("myModal");
+    modal.style.display = "block";
+}
+
+function saveName() {
+    const newName = document.getElementById("nameInput").value;
+    const headline = document.getElementById("headline");
+    if (newName) {
+        localStorage.setItem('userName', newName);
+        headline.innerText = newName;
+    }
+    closeModal();
+}
+
+function closeModal() {
+    const modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
+
+// Close modal when clicking outside of it
+window.onclick = function(event) {
+    const modal = document.getElementById("myModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
